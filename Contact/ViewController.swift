6051cd8fc29b929f19b30a81a0edde2db9413f4a
Amjad Khan <amjad.khan@conduent.com>
2018-12-23 13:50:35 +0530

    Initial Commit

---
+++ Contact/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contact
+//
+//  Created by Amjad Khan on 23/12/18.
+//  Copyright © 2018 GO-JEK Engineering. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
